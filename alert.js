b184751f99be1a6a757eb3eacb3bed0f2be79aa9
lapaweb <lapa-34@ya.ru>
2023-01-19 17:25:20 +0300

    Обновление по урокам

---
+++ alert.js
@@ -0,0 +1,35 @@
+// LJ
+// lesson1
+  // ex2
+
+  //   alert( 'Я Прекрасный JavaScript!' );
+
+// lesson2
+  // ex1
+
+    // let names="Джон";
+    // let admin=names
+
+    // alert(admin);
+
+  // ex2
+
+    // let OurPlanet;
+    // let currentUserName;
+
+//lsson3
+  // let name = "Ilya";
+
+  // alert( `hello ${1}` );
+  // alert( `hello ${"name"}` );
+  // alert( `hello ${name}` );
+
+
+
+
+
+
+
+
+
+
