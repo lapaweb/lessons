f0e9d8a6d0c9bebbdc1c95fd1c586217bec5cb39
lapaweb <lapa-34@ya.ru>
2023-01-20 18:21:53 +0300

    Урок 6. Взаимодействие

--- alert.js
+++ alert.js
@@ -17,13 +17,29 @@
     // let OurPlanet;
     // let currentUserName;
 
-//lsson3
+//lesson4
   // let name = "Ilya";
 
   // alert( `hello ${1}` );
   // alert( `hello ${"name"}` );
   // alert( `hello ${name}` );
 
+// lesson6
+
+  // ex1
+  // result = prompt(title, [default]); 
+
+  // ex2
+  // let age = prompt('Сколько тебе лет?', 100);
+  // alert(`Тебе ${age} лет!`);
+
+  // ex3
+  // let isBoss = confirm("Ты здесь главный?");
+  // alert(isBoss); // true, если нажата OK
+
+  // ex4
+  let UserName = prompt('Как тебя зовут?', "Ваня");
+  alert(`Меня зовут ${UserName} !`)
 
 
 
