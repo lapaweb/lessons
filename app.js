4d6deac0e1a269c8738e3f799a1df9f0681ba3e9
lapaweb <lapa-34@ya.ru>
2023-07-14 13:38:56 +0300

    lesson15 - function

--- app.js
+++ app.js
@@ -456,15 +456,77 @@ let progressNow;
 // }
 
 
+// lesson15
+
+// task2
+
+
+// let age = prompt ('Сколько вам лет?', '100');
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   } else {
+//     return confirm('Родители разрешили?');
+//   }
+// }
+
+// v1 - ||
+// function checkAge(age) {
+//   return (age > 18) || confirm('Родители разрешили?');
+// }
+
+// // v2 - ?
+// function checkAge(age) {
+//   return (age > 18) ? true :confirm('Родители разрешили?');
+// }
+
+// // checkAge(age);
+
+// // alert ( checkAge(age) )
+
+// if ( checkAge(age) ) {
+//   alert( 'Доступ получен' );
+// } else {
+//   alert( 'Доступ закрыт' );
+// }
+
+
+// task3
+
+// let a = prompt ('Введите первое число');
+// let b = prompt ('Введите второе число');
+
+// alert ( min (a, b) )
+
+// function min (a, b) {
+//   return (a < b) ? a : b;
+// }
+
+
+// task4
+
+// let x = prompt ('Введите число, которое нужно возвести в степень');
+// let n = prompt ('Введите степень');
+
+// // alert ( pow (x, n) )
+
+// function pow (x, n) {
+//   return x ** n;
+// }
+
+// if (n < 1) {
+//   alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+// } else {
+//   alert( pow(x, n) );
+// }
+ 
 
 
 
 
 
-
-
-
-// progressNow = 11;
+// progressNow = 17;
 // progressSum = (progressNow/lessonSum)*100;
 // alert(`Поздравляю, ты прошла ${progressSum}% курса!`);
 
