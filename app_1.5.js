3649669c4f4cd1401ec4758c08c4769fc6ac31c4
lapaweb <lapa-34@ya.ru>
2023-08-02 11:54:07 +0300

    lesson9 - Object.keys, values, entries

--- app_1.5.js
+++ app_1.5.js
@@ -815,6 +815,60 @@
 // alert(messagesReadMap.size);
 
 
+// lesson9 - Object.keys, values, entries
+
+
+// ex1 - fromEntries
+
+// let prices = {
+//   banana: 1,
+//   orange: 2,
+//   meat: 4,
+// };
+
+// let doublePrices = Object.fromEntries(
+//   // преобразовать в массив, затем map, затем fromEntries обратно объект
+//   // console.log(Object.entries(prices))
+//   Object.entries(prices).map(([key, value]) => [key, value * 2])
+// );
+
+// alert(doublePrices.meat); // 8
+
+
+// task1
+
+
+// let salaries = {
+//   "John": 100,
+//   "Pete": 300,
+//   "Mary": 250
+// };
+
+// function sumSalaries(obj) {
+//   let sum = 0;
+//   for (let value of Object.values(obj)) {
+//     sum = sum + value;
+//   }
+//   return sum;
+// }
+
+// alert( sumSalaries(salaries) ); // 650
+
+
+// task2
+
+
+// let user = {
+//   name: 'John',
+//   age: 30
+// };
+
+// function count(obj) {
+//   return Object.keys(obj).length;
+// }
+
+// alert( count(user) ); // 2
+
 
 
 
