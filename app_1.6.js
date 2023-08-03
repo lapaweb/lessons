debe4c0e719a6ddc4d15e9774f14dfa6ae5e711c
lapaweb <lapa-34@ya.ru>
2023-08-03 11:20:48 +0300

    create new file 1.6

---
+++ app_1.6.js
@@ -0,0 +1,11 @@
+// LJ part 1 section 6
+
+const lessonSum = 91;
+let progressSum;
+let progressNow;
+
+progressNow = 18+6+8+12;
+progressSum = ((progressNow/lessonSum)*100).toFixed(2);
+alert(`Поздравляю, ты прошла ${progressSum}% первой части курса!`);
+
+
